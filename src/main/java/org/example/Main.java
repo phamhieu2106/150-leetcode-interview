51a864b0effe6a7fea83928d0bf91678a07fc28a
Henry <henry.pham@alpaca.vn>
2025-06-17 12:02:12 +0700

    MergeTwoSortedArray

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
+import org.example.problem.MergeTwoSortedArray;
+
 public class Main {
     public static void main(String[] args) {
-        //Arrays
-//        RemoveDuplicatesFromSortedArray.removeDuplicatesFromSortedArray(); //TODO rethinking to understand
+        MergeTwoSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
     }
 }
---
+++ src/main/java/org/example/problem/MergeTwoSortedArray.java
@@ -0,0 +1,33 @@
+package org.example.problem;
+
+public class MergeTwoSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) { //TODO to understand
+
+    }
+
+
+    //TODO final code
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int nums1Index = m - 1;
+//        int nums2Index = n - 1;
+//        int insertIndex = m + n - 1;
+//
+//        while (nums1Index >= 0 && nums2Index >= 0) {
+//            if (nums1[nums1Index] > nums2[nums2Index]) {
+//                nums1[insertIndex] = nums1[nums1Index];
+//                nums1Index--;
+//            } else {
+//                nums1[insertIndex] = nums2[nums2Index];
+//                nums2Index--;
+//            }
+//            insertIndex--;
+//        }
+//
+//        while (nums2Index >= 0) {
+//            nums1[insertIndex] = nums2[nums2Index];
+//            nums2Index--;
+//            insertIndex--;
+//        }
+//    }
+}
