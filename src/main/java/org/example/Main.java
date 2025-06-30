0c8f3f5cabf57991fd5af4e484f4e34f0c8134ce
Henry <henry.pham@alpaca.vn>
2025-06-30 13:53:13 +0700

    init MergeTwoSortedArray

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.problem.MergeTwoSortedArray;
-
 public class Main {
     public static void main(String[] args) {
-        MergeTwoSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
     }
 }
--- src/main/java/org/example/problem/MergeTwoSortedArray.java
+++ src/main/java/org/example/problem/MergeTwoSortedArray.java
@@ -1,33 +1,38 @@
 package org.example.problem;
 
+import java.util.Arrays;
+
 public class MergeTwoSortedArray {
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) { //TODO to understand
+    /**
+     *
+     * @param nums1 array [1,1,1,0,0,0]
+     * @param m elements of nums1 {3}
+     * @param nums2 array [2,3,4]
+     * @param n elements of nums1 {3}
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int nums1Index = m - 1;
+        int nums2Index = n - 1;
+        int currentIndex = m + n - 1;
 
+        while (nums1Index >= 0 && nums2Index >= 0) {
+            if (nums1[nums1Index] > nums2[nums2Index]) {
+                nums1[currentIndex] = nums1[nums1Index];
+                nums1Index--;
+            } else {
+                nums1[currentIndex] = nums2[nums2Index];
+                nums2Index--;
+            }
+            currentIndex--;
+        }
+
+        while (nums2Index >= 0) {
+            nums1[currentIndex] = nums2[nums2Index];
+            nums2Index--;
+            currentIndex--;
+        }
+
+        System.out.println(Arrays.toString(nums1));
     }
-
-
-    //TODO final code
-//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-//        int nums1Index = m - 1;
-//        int nums2Index = n - 1;
-//        int insertIndex = m + n - 1;
-//
-//        while (nums1Index >= 0 && nums2Index >= 0) {
-//            if (nums1[nums1Index] > nums2[nums2Index]) {
-//                nums1[insertIndex] = nums1[nums1Index];
-//                nums1Index--;
-//            } else {
-//                nums1[insertIndex] = nums2[nums2Index];
-//                nums2Index--;
-//            }
-//            insertIndex--;
-//        }
-//
-//        while (nums2Index >= 0) {
-//            nums1[insertIndex] = nums2[nums2Index];
-//            nums2Index--;
-//            insertIndex--;
-//        }
-//    }
 }
